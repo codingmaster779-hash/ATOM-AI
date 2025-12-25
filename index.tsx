9998718c735f2341417ee68da4784aee842a31a6
codingmaster779-hash <codingmaster779@gmail.com>
2025-12-25 15:23:15 +0530

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import ErrorBoundary from './components/ErrorBoundary';
+import ErrorBoundary from './ErrorBoundary';
 import './index.css';
 
 const rootElement = document.getElementById('root');
